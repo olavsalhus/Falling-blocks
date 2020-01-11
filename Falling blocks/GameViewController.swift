6d2d52d0c888f27cb7273aca1bcb160282f5b01c
Olav Salhus <olav.salhus@gmail.com>
2020-01-12 00:26:51 +0100

    Initial Commit

---
+++ Falling blocks/GameViewController.swift
@@ -0,0 +1,40 @@
+//
+//  GameViewController.swift
+//  Blocks falling
+//
+//  Created by Olav Salhus on 05/01/2020.
+//  Copyright © 2020 Olav Salhus. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    let scene : GameScene = SKScene(fileNamed: "GameScene") as! GameScene
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            
+            scene.scaleMode = .aspectFit
+            
+            // Present the scene
+            view.presentScene(scene)
+        }
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .all
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
